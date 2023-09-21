d235f76b31a83dd57f2172ea0fd5d2afdea4dd45
nmejiab <nmejiab@unal.edu.co>
2023-09-21 15:05:12 -0500

    feat: if found a brown candie joint to a super candy

--- findMove.py
+++ findMove.py
@@ -260,6 +260,22 @@ def countMoves(candyMatrix, brownScore, explosiveMultiplier, verticalMultiplier,
             downExplosive = 0
             downVertical = 0
             downHorizontal = 0
+
+            # Verify if can be joint a brown candy with a super candy
+            if candyMatrix[i][j][0] == "C":
+                if i != matrixHeight - 1:
+                    if len(candyMatrix[i + 1][j]) == 2:
+                        return ([i, j], "d")
+                if i != 0:
+                    if len(candyMatrix[i - 1][j]) == 2:
+                        return ([i, j], "u")
+                if j != 0:
+                    if len(candyMatrix[i][j - 1]) == 2:
+                        return ([i, j], "l")
+                if j != matrixWidth - 1:
+                    if len(candyMatrix[i][j + 1]) == 2:
+                        return ([i, j], "r")
+                    
             # Verify if found 2 super candies joint
             if len(candyMatrix[i][j]) == 2:
                 if i != matrixHeight - 1:
